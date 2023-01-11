09c06b0b90c5b9777320c6a968bf92b2d0e8286b
Tien Dung <dungbuivm@gmail.com>
2023-01-11 08:07:51 +0700

    Bài Cấu Hình Router/Layout Dự Án TikTok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { Fragment } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { publicRoutes } from '~/rourtes'; // biến cấu hình router
+import DefaultLayout from '~/components/Layout/DefaultLayout';
 
 function App() {
-
   return (
-    
+    <BrowserRouter>
       <div className="App" style={{ padding: 20 }}>
-        
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            
+            const Page = route.component; // đặt biến để chuyển thành Components (trong bài JSX) vì bên dưới thằng element nó nhận vào là 1 Component
+            let Layout = DefaultLayout
+            if(route.layout){
+              Layout = route.layout
+            }else if(route.layout ===null){
+              Layout = Fragment
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
       </div>
-    
+    </BrowserRouter>
   );
 }
 
 export default App;
-
---
+++ src/rourtes/index.js
@@ -0,0 +1,26 @@
+//layouta
+import { HeaderOnly } from '~/components/Layout'
+
+//pages
+import Home from "~/pages/Home"
+import Following from "~/pages/Following"
+import Profile from "~/pages/Profile"
+import Upload from "~/pages/Upload"
+import Search from "~/pages/Search"
+
+//sử dụng cho các trang bình thường
+const publicRoutes =[
+  {path: '/', component: Home},
+  {path:'/following', component: Following},
+  {path:'/profile', component: Profile},
+  {path:'/search', component: Search, layout: null},
+  {path:'/upload', component: Upload, layout: HeaderOnly},
+]
+
+
+// những router sau này sử dụng cho các trang cần đăng nhập mới vào được
+const privateRoutes=[
+
+]
+
+export {publicRoutes ,privateRoutes}
